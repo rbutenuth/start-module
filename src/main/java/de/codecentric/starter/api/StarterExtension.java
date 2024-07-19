12b02cb64fe06748896b49906d363db4d88d3163
Roger Butenuth <roger.butenuth@codecentric.de>
2024-07-19 15:08:53 +0200

    Update for Mule Java SDK 1.6.3 / Java 17

--- src/main/java/de/codecentric/starter/api/StarterExtension.java
+++ src/main/java/de/codecentric/starter/api/StarterExtension.java
@@ -1,8 +1,13 @@
 package de.codecentric.starter.api;
 
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_11;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_17;
+import static org.mule.sdk.api.meta.JavaVersion.JAVA_8;
+
 import org.mule.runtime.extension.api.annotation.Extension;
 import org.mule.runtime.extension.api.annotation.Sources;
 import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
+import org.mule.sdk.api.annotation.JavaVersionSupport;
 
 /**
  * This is the main class of an extension, is the entry point from which configurations, connection providers, operations
@@ -11,6 +16,7 @@ import org.mule.runtime.extension.api.annotation.dsl.xml.Xml;
 @Xml(prefix = "starter")
 @Extension(name = "Starter")
 @Sources(RunOnceSource.class)
+@JavaVersionSupport({ JAVA_8, JAVA_11, JAVA_17})
 public class StarterExtension {
 	//
 }
